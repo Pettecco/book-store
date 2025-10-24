49a1ae6802921b4a1d233beaf3d8979a23b469bc
Pettecco <petterson.ikaro@gec.inatel.br>
2025-10-24 19:31:25 -0300

    feat(database): add MongoDB connection using Mongoose

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import connectDatabase from './config/dbConnect.js';
+import book from './models/book.js';
+
+const connection = await connectDatabase();
+
+connection.on('error', (error) => {
+  console.error('Erro de conexão', error);
+});
+
+connection.once('open', () => {
+  console.log('Conexão com o banco feita com sucesso');
+});
 
 const app = express();
 app.use(express.json()); //middleware
